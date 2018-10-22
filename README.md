26d58d72ea0e2f4dd93222db9b31e4bca7dffc93
joegeoffrey <geoffreypao@gmail.com>
2018-10-22 08:24:53 +0300

    added login and inventory pages [Deliver #161086572]

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a store manager web application that helps store owners manage sales and product inventory records. This application is meant for use in a single store.
---
+++ UI/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.nav-trigger').click(function() {
+		$('.side-nav').toggleClass('visible');
+	});
+});
+
+function toggle(source) {
+  checkboxes = document.getElementsByName('foo');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
